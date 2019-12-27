3e9ada303a3f9ef92e03d3d88eff53df6a7f24b8
Zeth Leonardo <zethiscool11@gmail.com>
2019-12-27 21:31:06 +0400

    Use prompts instead of CLI.

--- Hanabi.py
+++ Hanabi.py
@@ -1,7 +1,7 @@
 # Import our starters
 from sys import argv, exit
 from modules.modes import *
-from pyautogui import alert, press
+from pyautogui import alert, press, prompt
 
 def notify(mode):
     alert(f'Hanabi ~> Setting up { mode } mode, please wait...', title='Hanabi v3', timeout=3*1000)
@@ -11,16 +11,17 @@ def done():
 
 if __name__ == '__main__':
     # Get arguments
-    if len(argv) != 2:
-        exit('Please pass a valid argument.')
+    command = prompt('Hanabi ~> Please enter a mode.').lower()
+    if not command:
+        alert('Hanabi ~> No command passed, exiting!')
+        exit(0)
 
-    param = argv[1].lower()
-    if param == 'work':
-        notify(param)
+    if command == 'work':
+        notify(command)
         workMode()
         done()
 
-    elif param == 'chill':
-        notify(param)
+    elif command == 'chill':
+        notify(command)
         musicMode()
         done()
